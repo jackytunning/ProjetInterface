fb570bca9e23d236ced7ac80174ad42b3a27cf14
sullivan cobert <sullivan.cobert@laposte.net>
2021-03-03 00:53:17 +0100

    interface proto finie

--- ProjetInterface/src/com/projetInterface/View/View.java
+++ ProjetInterface/src/com/projetInterface/View/View.java
@@ -13,36 +13,40 @@ import javax.swing.*;
 public class View extends JFrame implements ActionListener{
 	//conteneur des menus
 	private JPanel menus;
-	private JPanel menuNav;
-		//boutons menu navigation
-		private JButton boutonJeux;
-		private JButton boutonUtilisateurs;
+		private JPanel menuNav;
+			//boutons menu navigation
+			private JButton boutonJeux;
+			private JButton boutonUtilisateurs;
+			
+		private JPanel menuJeu;
+			//contenu menu en jeu
 		
-	private JPanel menuJeu;
-		//contenu menu en jeu
-	
 	//conteneur de toutes les pages
 	private JPanel contenu;
+		private CardLayout card;
+		private JPanel panelJeux;
+			private JLabel labelJeux;
+			//ajouter les jeux
+		private JPanel panelJeu;
+			//contenu info jeu
+			private JLabel labelJeu;
+		private JPanel utilisateurs;
+			//liste des utilisateurs
+			//tableau?qui contient les utilisateurs(affichage)
+			private JLabel labelUtilisateurs;
+		private JPanel utilisateur;
+			private JLabel labelUtilisateur;
+			//infos de l'utilsateur
 	
-	private JPanel panelJeux;
-	private JLabel labelJeux;
-		//ajouter les jeux
-	private JPanel panelJeu;
-	private JLabel labelJeu;
-		//contenu info jeu
-	//liste des utilisateurs
-	private JPanel utilisateurs;
-		//tableau?qui contient les utilisateurs(affichage)
-		private JLabel labelUtilisateurs;
-	private JPanel utilisateur;
-		//infos de l'utilsateur
-	
-	CardLayout card;
+		
 	//conteneur general
 	private JPanel panel;
 	
 	private JButton boutonTemp;
 	private JButton boutonTemp2;
+	
+	private JButton boutonEx[] = new JButton[10];
+	private int i=0;
 	
 	
 	
@@ -91,35 +95,53 @@ public class View extends JFrame implements ActionListener{
 		this.menus.add(boutonUtilisateurs);
 		
 	}
+	private void initPanelJeux() {
+		this.boutonTemp= new JButton("Random Jeu");
+		this.boutonTemp.addActionListener(this);
+		this.panelJeux= new JPanel();
+		this.labelJeux = new JLabel("Jeux");
+		this.panelJeux.setLayout(new GridLayout(2,3));
+		this.panelJeux.add(labelJeux);
+		this.panelJeux.add(boutonTemp);
+	}
+	
+	private void initPanelJeu() {
+		this.panelJeu = new JPanel();
+		this.labelJeu = new JLabel("Jeu");
+		this.panelJeu.add(labelJeu);
+		this.boutonEx[i]= new JButton("Lancer une partie");
+		this.panelJeu.add(this.boutonEx[i]);
+		i++;
+		this.boutonEx[i]= new JButton("voir les onfos du jeu");
+		this.panelJeu.add(this.boutonEx[i] );
+		i++;
+	}
+	
+	private void initPanelUtilisateurs() {
+		this.utilisateurs= new JPanel();
+		this.utilisateurs.setLayout(new GridLayout(2,3));
+		this.boutonTemp2= new JButton("random user");
+		JButton spiderman = new JButton("nouvel user");
+		this.boutonTemp2.addActionListener(this);
+		this.labelUtilisateurs = new JLabel("Utilisateurs");
+		this.utilisateurs.add(labelUtilisateurs);
+		this.utilisateurs.add(spiderman);
+		this.utilisateurs.add(boutonTemp2);
+	}
+	
+	private void initPanelUtilisateur() {
+		this.utilisateur = new  JPanel();
+		this.labelUtilisateur = new JLabel("Utilisateur");
+		this.utilisateur.add(labelUtilisateur);		
+	}
+	
 	//initialise toutes les pages
 	private void initContenu() {
 		this.contenu = new JPanel();
-		this.panelJeu = new JPanel();
-		this.panelJeux= new JPanel();
-		this.utilisateur = new  JPanel();
-		this.utilisateurs= new JPanel();
-		
-		
-		this.boutonTemp= new JButton("Jeu");
-		this.boutonTemp.addActionListener(this);
-		
-		this.boutonTemp2= new JButton("random user");
-		this.boutonTemp2.addActionListener(this);
-		
-		//labels
-		this.labelJeux = new JLabel("Jeux");
-		this.panelJeux.add(labelJeux);
-		this.labelJeu = new JLabel("Jeu");
-		this.panelJeu.add(labelJeu);
-		this.labelUtilisateurs = new JLabel("Utilisateurs");
-		this.utilisateurs.add(labelUtilisateurs);
-		this.labelUtilisateur = new JLabel("Utilisateur");
-		this.utilisateur.add(labelUtilisateur);
-		
-		this.panelJeux.add(boutonTemp);
-		this.utilisateurs.add(boutonTemp2);
-		
-		
+		this.initPanelUtilisateurs();
+		this.initPanelJeu();
+		this.initPanelJeux();
+		this.initPanelUtilisateur();
 		this.contenu.setBounds(290,0,980,800);
 		this.card = new CardLayout();
 		this.contenu.setLayout(card);
